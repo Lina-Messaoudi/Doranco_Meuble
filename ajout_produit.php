b3979eeafa16ab44171e8221cfd544a3cffdbf2a
Lina Messaoudi <linaamessaoudi@gmail.com>
2022-11-17 15:04:24 +0100

    première initiation

---
+++ ajout_produit.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * 4 status de fichier sur git =
+ *      - untracked => les fichiers crée mais non suivies par git
+ *      - tracked => fichier deja suivies par git mais non envoyé vers le repository
+ *      - staged => les fichiers suivie, enregistré mais non envoyé vers le repository
+ *      - commited => tous les fichiers envoyé et non modifié de mon projet
+ */
